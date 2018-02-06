55d8a94911ddb5ee8a7b17c6f6e6d29b05b48770
Jean-Marc Prieur <jmprieur@microsoft.com>
2018-02-06 15:40:52 +0100

    - Improving the structure of code for the OpenIdConnect authentication notifications - replacing the email scope by Mail.Send

--- WebApp/App_Start/Startup.Auth.cs
+++ WebApp/App_Start/Startup.Auth.cs
@@ -25,7 +25,7 @@ namespace WebApp
         private static string appKey = ConfigurationManager.AppSettings["ida:ClientSecret"];
         public static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
         private static string redirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"];
-        
+
 
         public void ConfigureAuth(IAppBuilder app)
         {
@@ -41,7 +41,7 @@ namespace WebApp
                     RedirectUri = redirectUri
                 }
                 );
-          
+
             app.UseOpenIdConnectAuthentication(
                 new OpenIdConnectAuthenticationOptions
                 {
@@ -49,8 +49,8 @@ namespace WebApp
                     // The `Scope` describes the initial permissions that your app will need.  See https://azure.microsoft.com/documentation/articles/active-directory-v2-scopes/                    
                     ClientId = clientId,
                     Authority = String.Format(CultureInfo.InvariantCulture, aadInstance, "common", "/v2.0"),
-                    RedirectUri = redirectUri,                    
-                    Scope = "openid email profile offline_access Mail.Read",
+                    RedirectUri = redirectUri,
+                    Scope = "openid profile offline_access Mail.Read Mail.Send",
                     PostLogoutRedirectUri = redirectUri,
                     TokenValidationParameters = new TokenValidationParameters
                     {
@@ -66,35 +66,42 @@ namespace WebApp
                     },
                     Notifications = new OpenIdConnectAuthenticationNotifications
                     {
-                        // If there is a code in the OpenID Connect response, redeem it for an access token and refresh token, and store those away.
-                        AuthorizationCodeReceived = async (context) =>
-                        {
-                            var code = context.Code;
-                            string signedInUserID = context.AuthenticationTicket.Identity.FindFirst(ClaimTypes.NameIdentifier).Value;
-                            TokenCache userTokenCache = new MSALSessionCache(signedInUserID, 
-                                context.OwinContext.Environment["System.Web.HttpContextBase"] as HttpContextBase).GetMsalCacheInstance();                            
-                            ConfidentialClientApplication cca =
-                                new ConfidentialClientApplication(clientId, redirectUri, new ClientCredential(appKey), userTokenCache,null);
-                            string[] scopes = { "Mail.Read" };
-                            try
-                            {
-                                AuthenticationResult result = await cca.AcquireTokenByAuthorizationCodeAsync(code, scopes);
-                            }
-                            catch (Exception eee)
-                            {
-                                
-                            }
-                        },
-                        AuthenticationFailed = (notification) =>
-                        {
-                            notification.HandleResponse();
-                            notification.Response.Redirect("/Error?message=" + notification.Exception.Message);
-                            return Task.FromResult(0);
-                        }
+                        AuthorizationCodeReceived = OnAuthorization,
+                        AuthenticationFailed = OnAuthenticationFailed
+
                     }
                 });
         }
 
-     
+        private async Task OnAuthorization(AuthorizationCodeReceivedNotification context)
+        {
+            var code = context.Code;
+            string signedInUserID = context.AuthenticationTicket.Identity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            TokenCache userTokenCache = new MSALSessionCache(signedInUserID,
+                context.OwinContext.Environment["System.Web.HttpContextBase"] as HttpContextBase).GetMsalCacheInstance();
+            ConfidentialClientApplication cca =
+                new ConfidentialClientApplication(clientId, redirectUri, new ClientCredential(appKey), userTokenCache, null);
+            string[] scopes = { "Mail.Read" };
+            try
+            {
+                AuthenticationResult result = await cca.AcquireTokenByAuthorizationCodeAsync(code, scopes);
+            }
+            catch (Exception eee)
+            {
+
+            }
+        }
+
+        private Task OnAuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
+        {
+            // If there is a code in the OpenID Connect response, redeem it for an access token and refresh token, and store those away.
+            {
+                notification.HandleResponse();
+                notification.Response.Redirect("/Error?message=" + notification.Exception.Message);
+                return Task.FromResult(0);
+            }
+        }
+
+
     }
 }
