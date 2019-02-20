2555162df99ec284477eb3227670c67755883457
Jean-Marc Prieur <jmprieur@microsoft.com>
2019-02-20 14:12:44 +0100

    Updating to MSAL 2.7.0

--- WebApp/Models/MSALSessionCache.cs
+++ WebApp/Models/MSALSessionCache.cs
@@ -62,9 +62,6 @@ namespace WebApp_OpenIDConnect_DotNet.Models
         {
             SessionLock.EnterWriteLock();
 
-            // Optimistically set HasStateChanged to false. We need to do it early to avoid losing changes made by a concurrent thread.
-            cache.HasStateChanged = false;
-
             // Reflect changes in the persistent store
             httpContext.Session[CacheId] = cache.Serialize();
             SessionLock.ExitWriteLock();
@@ -81,7 +78,7 @@ namespace WebApp_OpenIDConnect_DotNet.Models
         void AfterAccessNotification(TokenCacheNotificationArgs args)
         {
             // if the access operation resulted in a cache update
-            if (cache.HasStateChanged)
+            if (args.HasStateChanged)
             {
                 Persist();
             }
