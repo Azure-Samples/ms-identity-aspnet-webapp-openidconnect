09003abf35615f0dabe15a1ea0eae80a5f7425df
Jennyf19 <jeferrie@microsoft.com>
2022-10-11 16:07:17 -0700

    initial commit

--- WebApp/App_Start/Startup.Auth.cs
+++ WebApp/App_Start/Startup.Auth.cs
@@ -1,17 +1,10 @@
 ﻿using Microsoft.Identity.Client;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.Owin.Host.SystemWeb;
+using Microsoft.Identity.Web;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Notifications;
-using Microsoft.Owin.Security.OpenIdConnect;
 using Owin;
-using System;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using WebApp.Utils;
-using System.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
 
 namespace WebApp
 {
@@ -23,84 +16,13 @@ namespace WebApp
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
 
-            // Custom middleware initialization. This is activated when the code obtained from a code_grant is present in the querystring (&code=<code>).
-            app.UseOAuth2CodeRedeemer(
-                new OAuth2CodeRedeemerOptions
-                {
-                    ClientId = AuthenticationConfig.ClientId,
-                    ClientSecret = AuthenticationConfig.ClientSecret,
-                    RedirectUri = AuthenticationConfig.RedirectUri
-                });
+            app.AddMicrosoftIdentityWebApp(configureServices: services =>
+            {
+                services.Configure<ConfidentialClientApplicationOptions>(options => { options.RedirectUri = "https://localhost:44326/"; });
+                services.AddMicrosoftGraph();
+                services.AddInMemoryTokenCaches();
+            });
 
-            app.UseOpenIdConnectAuthentication(
-                new OpenIdConnectAuthenticationOptions
-                {
-                    // This is needed for PKCE and resposne type must be set to 'code'
-                    UsePkce = true,
-                    ResponseType = OpenIdConnectResponseType.Code,
-
-                    // The `Authority` represents the v2.0 endpoint - https://login.microsoftonline.com/common/v2.0
-                    Authority = AuthenticationConfig.Authority,
-                    ClientId = AuthenticationConfig.ClientId,
-                    RedirectUri = AuthenticationConfig.RedirectUri,
-                    PostLogoutRedirectUri = AuthenticationConfig.RedirectUri,
-                    Scope = AuthenticationConfig.BasicSignInScopes + " Mail.Read User.Read", // a basic set of permissions for user sign in & profile access "openid profile offline_access"
-                    TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = false,
-                        // In a real application you would use IssuerValidator for additional checks, like making sure the user's organization has signed up for your app.
-                        //     IssuerValidator = (issuer, token, tvp) =>
-                        //     {
-                        //        //if(MyCustomTenantValidation(issuer))
-                        //        return issuer;
-                        //        //else
-                        //        //    throw new SecurityTokenInvalidIssuerException("Invalid issuer");
-                        //    },
-                        //NameClaimType = "name",
-                    },
-                    Notifications = new OpenIdConnectAuthenticationNotifications()
-                    {
-                        AuthorizationCodeReceived = OnAuthorizationCodeReceived,
-                        AuthenticationFailed = OnAuthenticationFailed,
-                        RedirectToIdentityProvider = OnRedirectToIdentityProvider,
-                    },
-                    // Handling SameSite cookie according to https://docs.microsoft.com/en-us/aspnet/samesite/owin-samesite
-                    CookieManager = new SameSiteCookieManager(
-                                     new SystemWebCookieManager())
-                });
-        }
-
-        private  Task OnRedirectToIdentityProvider(RedirectToIdentityProviderNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> arg)
-        {
-            arg.ProtocolMessage.SetParameter("myNewParameter", "its Value");
-            return Task.CompletedTask;
-        }
-
-        private async Task OnAuthorizationCodeReceived(AuthorizationCodeReceivedNotification context)
-        {
-            context.TokenEndpointRequest.Parameters.TryGetValue("code_verifier", out var codeVerifier);
-
-            // Upon successful sign in, get the access token & cache it using MSAL
-            IConfidentialClientApplication clientApp = MsalAppBuilder.BuildConfidentialClientApplication();
-            AuthenticationResult result = await clientApp.AcquireTokenByAuthorizationCode(new[] { "Mail.Read User.Read" }, context.Code)
-                .WithSpaAuthorizationCode() //Request an authcode for the front end
-                .WithPkceCodeVerifier(codeVerifier) // Code verifier for PKCE
-                .ExecuteAsync();
-
-            HttpContext.Current.Session.Add("Spa_Auth_Code", result.SpaAuthCode);
-
-            // This continues the authentication flow using the access token and id token retrieved by the clientApp object after
-            // redeeming an access token using the access code.
-            //
-            // This is needed to ensure the middleware does not try and redeem the received access code a second time.
-            context.HandleCodeRedemption(result.AccessToken, result.IdToken);
-        }
-
-        private Task OnAuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
-        {
-            notification.HandleResponse();
-            notification.Response.Redirect("/Error?message=" + notification.Exception.Message);
-            return Task.FromResult(0);
         }
     }
 }
