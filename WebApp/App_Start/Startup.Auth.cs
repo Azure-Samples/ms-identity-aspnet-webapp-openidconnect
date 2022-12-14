530922d77c1b09cf215d1269ac11f8a1cee03769
Jean-Marc Prieur <jmprieur@microsoft.com>
2022-12-13 20:12:35 -0800

    Update to Id.Web 2.0.8-preview

--- WebApp/App_Start/Startup.Auth.cs
+++ WebApp/App_Start/Startup.Auth.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.Identity.Client;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Client;
 using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.OWIN;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
+using Microsoft.IdentityModel.Validators;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
-using Microsoft.IdentityModel.Validators;
 
 namespace WebApp
 {
@@ -17,17 +18,25 @@ namespace WebApp
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
 
-            app.AddMicrosoftIdentityWebApp(configureServices: services =>
-            {
-                services.Configure<ConfidentialClientApplicationOptions>(options => { options.RedirectUri = "https://localhost:44326/"; });
-                services.AddMicrosoftGraph();
-                services.AddInMemoryTokenCaches();
-            },
-            updateOptions: options=>
+            // Get an TokenAcquirerFactory specialized for OWIN
+            OwinTokenAcquirerFactory owinTokenAcquirerFactory = TokenAcquirerFactory.GetDefaultInstance<OwinTokenAcquirerFactory>();
+
+            // Configure the web app.
+            app.AddMicrosoftIdentityWebApp(owinTokenAcquirerFactory,
+            updateOptions: options =>
             {
                 options.TokenValidationParameters.IssuerValidator = AadIssuerValidator.GetAadIssuerValidator(options.Authority).Validate;
             });
 
+            // Add the services you need.
+            owinTokenAcquirerFactory.Services
+                 .Configure<ConfidentialClientApplicationOptions>(options => { options.RedirectUri = "https://localhost:44326/"; })
+                .AddMicrosoftGraph()
+                .AddInMemoryTokenCaches();
+
+            owinTokenAcquirerFactory.Build();
+
+              
         }
     }
 }
